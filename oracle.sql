4d45a0973ad2accc4f630ea58f4a6d66a57f9ebe
COM <COM@DESKTOP-TH36TT5>
2022-03-30 14:31:42 +0900

    insert data

--- oracle.sql
+++ oracle.sql
@@ -9,3 +9,14 @@ password    varchar2(128) not null
 );
 
 create sequence board_no_seq;
+
+-- 10002건 데이터 입력
+begin
+for i in 1..10002 loop
+   insert into board
+   (no, title, name, password, content, regdate, readcount)
+   values(board_no_seq.nextval, '제목','이름','1111','내용..',
+   sysdate-((10002-i)/24/6), trunc(dbms_random.value(0, 100)));
+end loop;
+end;
+/
